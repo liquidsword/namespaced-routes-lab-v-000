15223b72048f0f24b51c29c6c536ddbfc013055e
Auden Robertson <audenrobertson@gmail.com>
2018-06-03 19:42:41 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    resources :preferences, only: [:index, :update]
-  end
 
   resources :artists do
     resources :songs, only: [:index, :show]
   end
   resources :songs
+
+  namespace :admin do
+    resources :preferences, only: [:index, :update]
+  end
 
 end
