ed836e3c7a916831548a81fa49bb3a13ec050b25
Auden Robertson <audenrobertson@gmail.com>
2018-06-03 20:01:22 +0000

    automatically backed up by learn

--- db/migrate/20180603200018_create_preferences.rb
+++ db/migrate/20180603200018_create_preferences.rb
@@ -2,7 +2,9 @@ class CreatePreferences < ActiveRecord::Migration
   def change
     create_table :preferences do |t|
       t.string :artist_sort_order
-      t.string :string
+      t.string :song_sort_order
+      t.boolean :allow_create_songs
+      t.boolean :allow_create_artists
     end
   end
 end
